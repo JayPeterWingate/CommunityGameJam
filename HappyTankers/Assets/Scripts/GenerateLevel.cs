56442e541c463badffe77f43d84f9fa9204931eb
Halberd <elhalo63@gmail.com>
2019-08-25 13:55:31 +1000

    Merge? Please work...

---
+++ HappyTankers/Assets/Scripts/GenerateLevel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    private LevelManager m_manager;
+    [SerializeField] private Texture2D m_levelMap;
+    private int xSize = 24;
+    private int ySize = 14;
+    private List<GameObject> m_levelObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_manager = transform.root.GetComponent<LevelManager>();
+        m_levelObjects = new List<GameObject>();
+        if (m_levelMap != null)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                for (int y = 0; y < ySize; y++)
+                {
+                    SpawnObject(x, y);
+                }
+            }
+        }
+        Destroy(GetComponent<MeshRenderer>());
+        //SetActiveLevel(false);
+    }
+
+    private void SpawnObject(int x, int y)
+    {
+        GameObject prefab = m_manager.GetPrefabFromColor(m_levelMap.GetPixel(x, y));
+        if (prefab != null)
+        {
+            m_levelObjects.Add(Instantiate(prefab, transform.position + new Vector3(x - xSize / 2, 0, y - ySize / 2), transform.rotation));
+        }
+    }
+
+    public void SetActiveLevel(bool setting)
+    {
+        for (int i = 0; i < m_levelObjects.Count; i++)
+        {
+            m_levelObjects[i].SetActive(setting);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ HappyTankers/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class LevelManager : MonoBehaviour
+{
+    [Serializable]
+    private class levelObjectColorCoded
+    {
+        public Color colorCode;
+        public GameObject prefab;
+    }
+
+    [SerializeField] private levelObjectColorCoded[] m_levelObjectCodedPrefabs;
+
+    public GameObject GetPrefabFromColor(Color col)
+    {
+        if (col.a == 0) { return null; }
+        for (int i = 0; i < m_levelObjectCodedPrefabs.Length; i++)
+        {
+            if (col == m_levelObjectCodedPrefabs[i].colorCode)
+            {
+                return m_levelObjectCodedPrefabs[i].prefab;
+            }
+        }
+        return null;
+    }
+}
