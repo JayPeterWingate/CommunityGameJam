33169648691c468168c8a28dcd040c77e25bc900
Jay <jaypeterwingate@gmail.com>
2019-08-25 13:35:14 +1000

    META UPDATE

---
+++ HappyTankers/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+	public float leftDrive;
+	public float rightDrive;
+}
+
+public class PlayerScript : TankController
+{
+
+	private void Update()
+	{
+		
+	}
+}
---
+++ HappyTankers/Assets/Scripts/TankScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankScript : MonoBehaviour
+{
+	[SerializeField] Transform m_leftTread;
+	[SerializeField] Transform m_rightTread;
+ 	[SerializeField] TankController m_controller;
+	[SerializeField] float m_horsePower;
+	[SerializeField] float m_rotatePower;
+	Rigidbody m_body;
+
+
+	// Start is called before the first frame update
+    void Start()
+    {
+		m_body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		m_body.AddTorque(0, m_rotatePower * ( m_controller.leftDrive - m_controller.rightDrive), 0);
+		m_body.AddForceAtPosition(transform.TransformDirection(new Vector3(0,0, m_controller.leftDrive * m_horsePower)), m_leftTread.position);
+
+		m_body.AddForceAtPosition(transform.TransformDirection(new Vector3(0, 0, m_controller.rightDrive * m_horsePower)), m_rightTread.position);
+	}
+}
