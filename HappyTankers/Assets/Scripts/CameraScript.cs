8b14ea632fef7a3d5559611abe3e0e2db5d76e3a
Halberd <elhalo63@gmail.com>
2019-08-27 18:59:36 +1000

    Filter change!

---
+++ HappyTankers/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    Animator m_animator;
+    float m_timer = 0;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void RemoveFilter()
+    {
+        m_animator.SetBool("RemoveFilter",true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer>10)
+        {
+            RemoveFilter();
+            m_timer = -1000f;
+        }
+    }
+}
