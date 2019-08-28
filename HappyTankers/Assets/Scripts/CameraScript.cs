de46c6d4e95b8b00a20b431484d22e1a2f8b5bc0
Halberd <elhalo63@gmail.com>
2019-08-28 14:58:40 +1000

    Highway to the DANGERZOOOOONE

--- HappyTankers/Assets/Scripts/CameraScript.cs
+++ HappyTankers/Assets/Scripts/CameraScript.cs
@@ -6,15 +6,22 @@ public class CameraScript : MonoBehaviour
 {
     Animator m_animator;
     float m_timer = 0;
+    Vector3 m_targetPos;
 
     void Start()
     {
         m_animator = GetComponent<Animator>();
+        m_targetPos = transform.position;
     }
 
     public void RemoveFilter()
     {
         m_animator.SetBool("RemoveFilter",true);
+    }
+
+    public void LerpMoveFocus(Vector3 newPos)
+    {
+        m_targetPos = newPos;
     }
 
     // Update is called once per frame
@@ -25,6 +32,15 @@ public class CameraScript : MonoBehaviour
         {
             RemoveFilter();
             m_timer = -1000f;
+        }
+
+        if ((m_targetPos - transform.position).magnitude < 0.1f)
+        {
+            transform.position += (m_targetPos - transform.position) * 1 * Time.deltaTime;
+        }
+        else
+        {
+            m_targetPos = transform.position;
         }
     }
 }
--- HappyTankers/Assets/Scripts/LevelManager.cs
+++ HappyTankers/Assets/Scripts/LevelManager.cs
@@ -13,10 +13,21 @@ public enum CamPos
     BotHW,
 }
 
+public enum CamTransitionType
+{
+    HW_New,
+    HW_Old,
+    Room_Up,
+    Room_Down,
+    Room_Left,
+    Room_Right,
+}
+
+[Serializable]
 public struct LevelGridElements
 {
-    public Vector3 worldCentre;
-    public CamPos camPos;
+    [HideInInspector] public Vector3 worldCentre;
+    [HideInInspector] public CamPos camPos;
     //Vertical Highways
     public GameObject HW_V11;
     public GameObject HW_V21;
@@ -53,8 +64,8 @@ public class LevelManager : MonoBehaviour
 	public static bool IsHappy { get; private set; }
     [SerializeField] private levelObjectColorCoded[] m_levelObjectCodedPrefabs;
 	[SerializeField] private LevelProgression[] m_levels;
-	[SerializeField] private GameObject m_camera;
-
+	[SerializeField] private CameraScript m_camera;
+    
     [SerializeField] private LevelGridElements m_gridElements;
 
     public GameObject GetPrefabFromColor(Color col)
@@ -77,16 +88,58 @@ public class LevelManager : MonoBehaviour
         m_gridElements.LevelCurrent = m_levels[0].gameObject;
         m_gridElements.LevelNext = m_levels[1].gameObject;
         m_gridElements.LevelCurrent.transform.position = new Vector3(0, 0, 0);
-        LegitTransition(0)();
+        m_gridElements.camPos = CamPos.InRoom;
+        m_gridElements.worldCentre = new Vector3(0,0,0);
+        FilteredTransition(0)();
 
 		for (int i = 0; i < m_levels.Length; i++)
 		{
-			m_levels[i].OnLevelComplete.AddListener(LegitTransition(i + 1));
+			m_levels[i].OnLevelComplete.AddListener(FilteredTransition(i + 1));
 		}
         
     }
 
-	private UnityAction LegitTransition(int i)
+    private void UnfilteredTransition(CamTransitionType type)
+    {
+        switch (type)
+        {
+            case CamTransitionType.HW_New:
+                {
+                    m_gridElements.camPos = CamPos.InRoom;
+
+                    break;
+                }
+            case CamTransitionType.HW_Old:
+                {
+                    m_gridElements.camPos = CamPos.InRoom;
+                    break;
+                }
+            case CamTransitionType.Room_Left:
+                {
+                    m_gridElements.camPos = CamPos.LeftHW;
+                    break;
+                }
+            case CamTransitionType.Room_Right:
+                {
+                    m_gridElements.camPos = CamPos.RightHW;
+                    break;
+                }
+            case CamTransitionType.Room_Up:
+                {
+                    m_gridElements.camPos = CamPos.TopHW;
+                    break;
+                }
+            case CamTransitionType.Room_Down:
+                {
+                    m_gridElements.camPos = CamPos.BotHW;
+                    break;
+                }
+            default: break;
+        }
+    }
+
+
+    private UnityAction FilteredTransition(int i)
 	{
 		return () =>
 		{
