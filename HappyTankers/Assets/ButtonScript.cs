c8f3185aab7d08f7017c763a5bd47b336080233f
Jay <jaypeterwingate@gmail.com>
2019-08-31 18:57:39 +1000

    Menu

---
+++ HappyTankers/Assets/ButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+	[SerializeField] Sprite selected;
+	[SerializeField] Sprite unselected;
+	[SerializeField] TMPro.TextMeshProUGUI m_text;
+	public void StartGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+    public void HoverButton()
+	{
+		Color newColor = Color.HSVToRGB(Random.value, 1, 1);
+		GetComponent<Image>().color = newColor; 
+		GetComponent<Image>().sprite = selected;
+		m_text.color = newColor;
+	}
+
+	public void UnHover()
+	{
+		Color newColor = new Color(1, 1, 1);
+		GetComponent<Image>().color = newColor;
+		GetComponent<Image>().sprite = unselected;
+		m_text.color = new Color();
+	}
+}
